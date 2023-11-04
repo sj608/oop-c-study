3f4f8a8f5d3734f0b55d1bce56d294a2e4b19dae
sj608 <steven_psj@hotmail.com>
2023-11-03 23:56:39 -0700

    following abstact data type example

--- oop-c/src/main.c
+++ oop-c/src/main.c
@@ -1,7 +1,27 @@
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdio.h>
+#include "new.h"
+#include "set.h"
+#include "object.h"
 
 int main(void)
 {
+    void *s = new(set);
+    void *a = add(s, new(object));
+    void *b = add(s, new(object));
+    void *c = new(object);
+    if (contains(s, a) && contains(s, b))
+        puts("ok");
+    if(contains(s,c))
+        puts("contains?");
+    if(differ(a, add(s,a)))
+        puts("differ?");
+    if(contains(s, drop(s,a)))
+        puts("drop?");
+    delete(drop(s,b));
+    delete(drop(s,c));
+    
+
     return 0;
 }
---
+++ oop-c/src/object.h
@@ -0,0 +1,7 @@
+#ifndef OBJ_H
+#define OBJ_H
+
+extern const void *object;
+int differ (const void *a, const void *b);
+
+#endif
